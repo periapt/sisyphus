1d3595f1e472f3dbd18a8d34094e7c60df55e289
Nicholas Bamber <nicholas@periapt.co.uk>
2012-06-11 12:37:20 +0100

    wrote basic intro in Sisyphus.pm and generated README from it

--- lib/Sisyphus.pm
+++ lib/Sisyphus.pm
@@ -6,7 +6,7 @@ use warnings;
 
 =head1 NAME
 
-Sisyphus - The great new Sisyphus!
+Sisyphus - automated Debian pre-upload testing
 
 =head1 VERSION
 
@@ -19,78 +19,21 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Before uploading a Debian packages a number of tests should ideally 
+be completed successfully: building from scratch, lintian cleanliness,
+piuparts, blhc, building dependent packages and other application
+specific tests. In order to catch issues as early as possible it would
+be nice to be able to run these tests against what is currently checked
+into the repository.
 
-Perhaps a little code snippet.
-
-    use Sisyphus;
-
-    my $foo = Sisyphus->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+The tests are done in a schroot environment that the script keeps uptodate.
+The tests are configurable so can be adapted to specific needs.
+Tests have dependencies which can be both other tests and anything
+that can be coded as a Perl function.
 
 =head1 AUTHOR
 
 Nicholas Bamber, C<< <nicholas at periapt.co.uk> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-sisyphus at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Sisyphus>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Sisyphus
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Sisyphus>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Sisyphus>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Sisyphus>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Sisyphus/>
-
-=back
-
 
 =head1 ACKNOWLEDGEMENTS
 
